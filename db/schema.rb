c7abced40d1c7d0747faef53d41408ce4d14d081
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2023-11-13 15:51:20 +1100

    update

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_13_011023) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
